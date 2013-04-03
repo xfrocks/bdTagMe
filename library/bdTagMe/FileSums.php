9a14449fff14cb77df87f18daa6a462ecd12006f
Dao Hoang Son <daohoangson@gmail.com>
2013-04-04 03:29:22 +0700

    v1.7.1 -- version bump

--- library/bdTagMe/FileSums.php
+++ library/bdTagMe/FileSums.php
@@ -5,14 +5,14 @@ class bdTagMe_FileSums
 	public static function getHashes()
 	{
 		return array (
-  'library/bdTagMe/Engine.php' => 'fc76e20e10fddf520987a943d83491b8',
+  'library/bdTagMe/Engine.php' => '639067890f07178ef177229ae8c39d8b',
   'library/bdTagMe/Installer.php' => 'bb0afad3d40a88b67993edf52aa85271',
-  'library/bdTagMe/Listener.php' => '56247b0b8a04eb177b235ab351b3a3fe',
+  'library/bdTagMe/Listener.php' => '2c6829da1dc52471ba88f093ced2b3cb',
   'library/bdTagMe/Option.php' => '448e6e18444b16b9d82bd85ec3bc5cc4',
   'library/bdTagMe/Template/Helper.php' => '1e48720d1143142e08b2a064490c8636',
   'library/bdTagMe/XenForo/BbCode/Formatter/Base.php' => '9c2a653bb2b177153f6c83bd95ec07ce',
   'library/bdTagMe/XenForo/ControllerAdmin/UserGroup.php' => '3d27724080d9ee0aca1e2fc89b498f15',
-  'library/bdTagMe/XenForo/ControllerPublic/Account.php' => '4e67449de7a58960fada225a1c9d316e',
+  'library/bdTagMe/XenForo/ControllerPublic/Account.php' => '93fd9b3ffd2df5a83fb22484de7ccd20',
   'library/bdTagMe/XenForo/ControllerPublic/Member.php' => 'd07c70f73b32ab7fedf0270aed987dea',
   'library/bdTagMe/XenForo/DataWriter/DiscussionMessage/Post.php' => '4a4047ff18e98e4185a50363924ca269',
   'library/bdTagMe/XenForo/DataWriter/DiscussionMessage/ProfilePost.php' => '815261b173604b16139f6cdbd8b2584d',
@@ -25,10 +25,10 @@ class bdTagMe_FileSums
   'library/bdTagMe/XenForo/Model/User.php' => '9ee89f1094363c01dd31743eda968484',
   'library/bdTagMe/XenForo/Route/Prefix/Members.php' => 'ca64b4d980a788365bae15a0151a8fbf',
   'library/bdTagMe/XenForo/ViewPublic/Member/Post.php' => '5f31a5c7e4fc8f91fcc582b66662b622',
-  'js/bdTagMe/frontend.js' => '0d3e62f887bd3f44bddacd900311ad9c',
-  'js/bdTagMe/full/frontend.js' => '251572736dfa6c0dd24611e50d41c87a',
-  'js/bdTagMe/full/tinymce_plugin.js' => '53efef7d966323038bb53114a704e3e6',
-  'js/bdTagMe/tinymce_plugin.js' => 'a56f44c7a5ef935958571538de0c5579',
+  'js/bdTagMe/frontend.js' => '7ea1e8e26ce286f72ec58250d99038b3',
+  'js/bdTagMe/full/frontend.js' => '4fb70360452f57f27210aae7f2f8045c',
+  'js/bdTagMe/full/tinymce_plugin.js' => '945c126f657a22a4de105750caf3168b',
+  'js/bdTagMe/tinymce_plugin.js' => '2e93419ef8bc2738be819519d9ad6b6b',
 );
 	}
 }
