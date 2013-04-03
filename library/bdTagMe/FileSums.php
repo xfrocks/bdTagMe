edb5865447c7fcc150b67b806c75adfb33758d9a
Dao Hoang Son <daohoangson@gmail.com>
2013-04-04 02:28:20 +0700

    v1.6.6 -- version bump

--- library/bdTagMe/FileSums.php
+++ library/bdTagMe/FileSums.php
@@ -5,7 +5,7 @@ class bdTagMe_FileSums
 	public static function getHashes()
 	{
 		return array (
-  'library/bdTagMe/Engine.php' => '350d64897debff46f71a2aadf6077ad1',
+  'library/bdTagMe/Engine.php' => 'a784137fc75dfce59f439970ad6bed0d',
   'library/bdTagMe/Listener.php' => '8be8d3518c8e15a00f2e399ff16af98b',
   'library/bdTagMe/Option.php' => '345a57139d4c4f4553114471aa2478c2',
   'library/bdTagMe/Template/Helper.php' => '1e48720d1143142e08b2a064490c8636',
@@ -17,7 +17,7 @@ class bdTagMe_FileSums
   'library/bdTagMe/XenForo/DataWriter/ProfilePostComment.php' => '80d47f52624b69b13fd35d09024d374e',
   'library/bdTagMe/XenForo/DataWriter/User.php' => 'e84bd33415814c6e0eaa671c9e234ae9',
   'library/bdTagMe/XenForo/DataWriter/UserGroup.php' => '0d3e64c97ee6a172587853fc1be5d812',
-  'library/bdTagMe/XenForo/Model/Alert.php' => 'c131537951bc5c46442de2f6e62c59f1',
+  'library/bdTagMe/XenForo/Model/Alert.php' => 'bd3836ffb950906628dbfcf10d11f656',
   'library/bdTagMe/XenForo/Model/Post.php' => 'fdf58cdc19fbdbe593876e0908bf8253',
   'library/bdTagMe/XenForo/Model/ProfilePost.php' => '1dcb15c1b0c1f7841337427f563be7a1',
   'library/bdTagMe/XenForo/Model/User.php' => '9ee89f1094363c01dd31743eda968484',
